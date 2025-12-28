98497f0f9aa1734a3305580d1242f8d23602c5ab
Mori Yuta <yuta.m.12.1214@gmail.com>
2025-12-28 21:54:13 +0900

    テンプレート用にコメント追加

--- abc431/a/main.js
+++ abc431/a/main.js
@@ -1,8 +1,9 @@
 function main(input) {
+    // 入力の受け取り
 	const [d, f] = input.split(" ").map(Number);
-
+    // ロジックの実装
     const result = 7 - (d - f) % 7;
-
+    // 結果の出力`
 	console.log(result);
 }
 
